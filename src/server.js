f0670929ec2ae71111a31d42b8229b926e18df07
Jaeyoung <jyko1101@gmail.com>
2025-01-08 17:05:59 +0900

    feat: server.js

---
+++ src/server.js
@@ -0,0 +1,23 @@
+//====================================================================================================================
+//====================================================================================================================
+// server.js
+// 서버의 메인 진입점
+//====================================================================================================================
+//====================================================================================================================import net from 'net';
+import { onConnection } from './events/onConnection';
+import initServer from './init';
+import { config } from 'dotenv';
+
+const server = net.createServer(onConnection);
+
+initServer()
+    .then(() => {
+        server.listen(config.server.port, config.server.host, () => {
+            console.log(`Server is running on ${config.server.host}:${config.server.port}`);
+            console.log(server.address());
+        });
+    })
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
