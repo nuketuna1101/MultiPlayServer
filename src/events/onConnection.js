eda27946e1970484fa7d2ed3edfc8f2a9f37aaab
Jaeyoung <jyko1101@gmail.com>
2025-01-10 15:54:36 +0900

    chore: cleansing

--- src/events/onConnection.js
+++ src/events/onConnection.js
@@ -9,11 +9,12 @@ import { onError } from './onError.js';
 import { onData } from './onData.js';
 
 export const onConnection = (socket) => {
-    console.log('Client on Connected : ', socket.remoteAddress, socket.remotePort);
+    console.log('[onConnection] Client on Connected : ', socket.remoteAddress, socket.remotePort);
 
     // 각 client가 buffer 고유하게 유지하도록 할당
     socket.buffer = Buffer.alloc(0);
 
+    //
     socket.on('data', onData(socket));
     socket.on('end', onEnd(socket));
     socket.on('error', onError(socket));
